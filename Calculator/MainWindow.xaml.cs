6be7422eceb28aa7405d79efb39513ce656f20f9
Naduni Pamudika <NaduniP@embla.lk>
2015-12-17 11:48:10 +0530

    Code Refined

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -33,48 +33,6 @@ namespace Calculator
             InitializeComponent();
             DBConnection dbc = DBConnection.GetInstance();
             operations = dbc.GetAccess("CalculatorDB", "Operations");
-            //InsertData();
-        }
-
-        private void InsertData()
-        {
-            var cal = new Cal();
-            cal.num1 = 2;
-            cal.num2 = 3;
-            cal.result = 5;
-            operations.InsertOne(cal);
-
-            BsonDocument data = new BsonDocument {
-            { "address" , new BsonDocument
-                {
-                    { "street", "2 Avenue" },
-                    { "zipcode", "10075" },
-                    { "building", "1480" },
-                    { "coord", new BsonArray { 73.9557413, 40.7720266 } }
-                }
-            },
-            { "borough", "Manhattan" },
-            { "cuisine", "Italian" },
-            { "grades", new BsonArray
-                {
-                    new BsonDocument
-                    {
-                        { "date", new DateTime(2014, 10, 1, 0, 0, 0, DateTimeKind.Utc) },
-                        { "grade", "A" },
-                        { "score", 11 }
-                    },
-                    new BsonDocument
-                    {
-                        { "date", new DateTime(2014, 1, 6, 0, 0, 0, DateTimeKind.Utc) },
-                        { "grade", "B" },
-                        { "score", 17 }
-                    }
-                }
-            },
-            { "name", "Vella" },
-            { "restaurant_id", "41704620" } };
-            //var collection = database.GetCollection<BsonDocument>("restaurants");
-            //collection.InsertOneAsync(data);
         }
 
         /// <summary>
